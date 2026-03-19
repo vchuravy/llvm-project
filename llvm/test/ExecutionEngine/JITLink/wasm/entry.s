4e5175c55fc7d3582c790462f60515deaa87e424
Valentin Churavy <v.churavy@gmail.com>
2026-03-19 11:30:38 +0100

    fixup! fixup! fixup! prototype wasm support

--- llvm/test/ExecutionEngine/JITLink/wasm/entry.s
+++ llvm/test/ExecutionEngine/JITLink/wasm/entry.s
@@ -1,6 +1,7 @@
 # RUN: llvm-mc -filetype=obj -triple=wasm32-unknown-emscripten -o %t.o %s
 # RUN: llvm-jitlink -triple=wasm32-unknown-emscripten -entry=entry -noexec -check %s %t.o
 
+# jitlink-check: *{1}entry = 0xb
 .globl  entry
 entry:
   .functype entry () -> ()
